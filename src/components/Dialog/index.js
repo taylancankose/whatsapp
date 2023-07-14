1883800220d1ee1d39f79501b4a62c6938f65366
Taylan Kose <taylancankose@gmail.com>
2023-07-14 23:42:27 +0300

    Feature: Message Bubble and Message added

---
+++ src/components/Dialog/index.js
@@ -0,0 +1,45 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { StyleSheet } from "react-native";
+import useTimeFormatter from "../../hooks/useTimeFormatter";
+
+const Dialog = ({ message }) => {
+  const isMyMessage = () => {
+    return (message.user.id = "u1");
+  };
+  console.log(message.user.id);
+
+  return (
+    <View
+      style={[
+        styles.container,
+        message.user.id === "u1"
+          ? {
+              backgroundColor: "#DCF8C5",
+              alignSelf: "flex-end",
+            }
+          : {
+              backgroundColor: "white",
+              alignSelf: "flex-start",
+            },
+      ]}
+    >
+      <Text>{message.text}</Text>
+      <Text style={styles.time}>{useTimeFormatter(message.createdAt)}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 5,
+    padding: 10,
+    borderRadius: 10,
+    maxWidth: "80%",
+  },
+  time: {
+    color: "gray",
+    alignSelf: "flex-end",
+  },
+});
+export default Dialog;
---
+++ src/screens/MessageScreen/index.jsx
@@ -0,0 +1,39 @@
+import {
+  View,
+  Text,
+  ImageBackground,
+  StyleSheet,
+  FlatList,
+} from "react-native";
+import React from "react";
+import bg from "../../../assets/images/BG.png";
+import Dialog from "../../components/Dialog";
+import messages from "../../../assets/data/messages.json";
+
+const MessageScreen = () => {
+  console.log(messages, "s");
+
+  return (
+    <ImageBackground source={bg} style={styles.bg}>
+      <FlatList
+        keyExtractor={(item) => item.id}
+        inverted
+        data={messages}
+        style={styles.list}
+        renderItem={({ item }) => <Dialog message={item} />}
+        showsVerticalScrollIndicator={false}
+      />
+    </ImageBackground>
+  );
+};
+
+const styles = StyleSheet.create({
+  bg: {
+    flex: 1,
+  },
+  list: {
+    padding: 10,
+  },
+});
+
+export default MessageScreen;
